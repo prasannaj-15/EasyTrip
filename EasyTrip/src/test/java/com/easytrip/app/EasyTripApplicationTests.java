f57bac32f40f3f6a9016807fbeeddfabdf5951e7
prasannaj-15 <prasanna15j@gmail.com>
2023-01-18 09:42:17 +0530

    added base structure of project

---
+++ EasyTrip/src/test/java/com/easytrip/app/EasyTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easytrip.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
