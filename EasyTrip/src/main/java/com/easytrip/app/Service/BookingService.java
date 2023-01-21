1dd0bf2ae662e642a5c127cce4eb68d08517385f
0786faizan <fchauhan114@gmail.com>
2023-01-21 15:20:00 +0530

    booking servies added

--- EasyTrip/src/main/java/com/easytrip/app/Service/BookingService.java
+++ EasyTrip/src/main/java/com/easytrip/app/Service/BookingService.java
@@ -1,8 +1,14 @@
 package com.easytrip.app.Service;
 
+import java.util.Set;
+
 import com.easytrip.app.Exception.BookingException;
+import com.easytrip.app.Exception.HotelException;
 import com.easytrip.app.Exception.PackageException;
 import com.easytrip.app.Model.Booking;
+import com.easytrip.app.Model.Customer;
+import com.easytrip.app.Model.Hotel;
+import com.easytrip.app.Model.TripPackage;
 
 public interface BookingService {
 
@@ -10,5 +16,9 @@ public interface BookingService {
 	  
 	  public Booking cancelBooking(Booking book)throws BookingException;
 	  
-	  public Booking viewBooking(Integer bookingId);
+	  public Booking viewBooking(Integer bookingId) throws BookingException;
+	  
+	public Set<Booking> assignCustomertoBooking(Customer customer, Integer customerId)throws BookingException;  	  
+	  	  
+
 }
